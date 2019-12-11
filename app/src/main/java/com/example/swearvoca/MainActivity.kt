22c836d9728ab12d03ca14e5ba3fdb5ca70f53e9
Jyeontae <jyt5768@hanmail.net>
2019-12-11 23:46:04 +0900

    24시간 후에 메인액티비티 보이게 함

--- app/src/main/java/com/example/swearvoca/MainActivity.kt
+++ app/src/main/java/com/example/swearvoca/MainActivity.kt
@@ -3,21 +3,33 @@ package com.example.swearvoca
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.Month
 
 class MainActivity : AppCompatActivity() {
-
+    var todayY:Int = 0
+    var todayM: Int = 0
+    var todayD:Int =0
+    var todayHour: Int = 0
+    var todayMinute: Int =0
+    var wordcount: Int = 30
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setTitle("학습 설정")
 
+        loadTimeData()
+
         var init_Time : String = "00시 00분"
         gasu.setText("30")
-        var wordcount : Int = gasu.text.toString().toInt() //단어개수 디폴트 30
+        wordcount  = gasu.text.toString().toInt() //단어개수 디폴트 30
         var total_word: Int = loadData()
         gasu.setText("$wordcount")
 
@@ -37,6 +49,8 @@ class MainActivity : AppCompatActivity() {
         timePicker.setOnTimeChangedListener { timePicker, i, i2 ->
             Toast.makeText(this, "${i}시 ${i2}분으로 설정되었습니다.", Toast.LENGTH_SHORT).show()
             init_Time = "${i}시 ${i2}분"
+            todayHour = i
+            todayMinute = i2
         } //시간 설정 이벤트
 
         check.setOnClickListener {
@@ -45,11 +59,16 @@ class MainActivity : AppCompatActivity() {
         } // 설정확인 버튼 처리
 
         start.setOnClickListener {
+            todayY = LocalDateTime.now().year
+            todayM = LocalDateTime.now().monthValue
+            todayD = LocalDateTime.now().dayOfMonth
+            saveTimeData(todayY, todayM, todayD, todayHour, todayMinute)
             total_word += wordcount
             saveData(total_word)
             startActivity<study>(
                     "word_count" to wordcount
-            )
+            )//단어
+
             finish()
         } //시작 이벤트 처리
     }
@@ -65,5 +84,34 @@ class MainActivity : AppCompatActivity() {
         val pref = PreferenceManager.getDefaultSharedPreferences(this)
         var wordcount = pref.getInt("KEY_WORDCOUNT",0)
         return wordcount
+    }
+
+    private fun saveTimeData(y: Int, m: Int, d: Int, h: Int, m2: Int){
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val editor = pref.edit()
+        editor.putInt("KEY_YEAR", y)
+            .putInt("KEY_MONTH", m)
+            .putInt("KEY_DAY", d)
+            .putInt("KEY_HOUR", h)
+            .putInt("KEY_MINUTE", m2)
+            .apply()
+    }
+
+    private fun loadTimeData(){
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+            var s1 =pref.getInt("KEY_YEAR", 2019)
+            var s2 = pref.getInt("KEY_MONTH", 1)
+            var s3 = pref.getInt("KEY_DAY", 1)
+            var s4 = pref.getInt("KEY_HOUR", 0)
+            var s5 = pref.getInt("KEY_MINUTE", 0)
+            //var dateTime = LocalDateTime.of(s1, s2, s3, s4, s5).plusDays(1)
+            var dateTime = LocalDateTime.of(s1, s2, s3, s4, s5) //시간 설정의시간
+        Log.d("date", dateTime.toString())
+        if(dateTime >= LocalDateTime.now()){
+            startActivity<study>(
+                "word_count" to wordcount
+            )
+            finish()
+        }
     }
 }
